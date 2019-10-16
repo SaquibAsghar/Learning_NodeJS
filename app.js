b024b920dd7f1c4103b4d8f69de77703777dba74
SaquibAsghar <saq9516@gmail.com>
2019-10-16 19:40:29 +0530

    Update app.js
    In this request url is storred in const url and a if condition is applied to navigate to the '/form' page where a form is created with one input and a submit button. Sending the form input(s) via POST method define in the form tag as an attribute and action equals to '/formMessage'.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+	const url = req.url;
+	if(url === '/form'){
+		res.write("<html>");
+		res.write("<head><title>My first form</title></head>");
+		res.write('<body><h1>Form</h1><form action="/formMessage" method = "POST" ><input type="text" name="userInput" /><button type ="submit" >click me</button></form></body>');
+		res.write("</html>");
+		return res.end();
+	}
+
+
 	res.setHeader("Content-type", "text/html"); // setting the header
 	res.write("<html>");
 	res.write("<head><title>My first title from response</title></head>");
