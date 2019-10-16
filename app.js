522bef9a33c08ca33b5b9482adad3a84067d48d4
SaquibAsghar <saq9516@gmail.com>
2019-10-16 15:03:15 +0530

    set the response, a simple html page with a h1 tag

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    console.log(req);
-    process.exit(); 
-})
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+	console.log(req.method);
+	console.log(req.headers);
+	res.setHeader("Content-type", "text/html"); // setting the header
+	res.write("<html>");
+	res.write("<head><title>My first title from response</title></head>");
+	res.write("<body><h1>Hello from Node.js server </h1></body>");
+	res.write("</html>");
+	res.end(); // to tell the node we are done setting the response :)
+    // process.exit();
+});
 
 server.listen(3005);
