4a1d7bb5ab7f0f09a1dd3d055755abcc48717626
SaquibAsghar <saq9516@gmail.com>
2019-10-22 20:57:04 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,13 @@ const express = require('express');
 const adminRoute = express.Router();
 adminRoute.get('/add-product',(req, res, next)=>{
     // console.log("In add-product page");
-    res.send("<form action=\"/product\" method = \"POST\" ><input type = \"text\" name = \"title\"><button type = \"submit\">Submit</button></form")
+    res.send("<form action=\"/admin/product\" method = \"POST\" ><input type = \"text\" name = \"title\"><button type = \"submit\">Submit</button></form")
     // next(); // allows the request to continue to the next middleware in line 
 });
 
 adminRoute.post('/product',(req, res, next)=>{
     console.log(req.body);
-    res.redirect('/');
+    res.redirect('/'); // this will route to the initial page
 });
 
 module.exports = adminRoute;
