a281cf654d6798d1d29a4b7d5d5313390c1ff2fa
SaquibAsghar <saq9516@gmail.com>
2019-10-22 14:12:39 +0530

    Update app.js
    a form response is send and is directed to new route after submiting the form.  The input name is taken and console the req.body in the /product

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express  = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product',(req, res, next)=>{
-    console.log("In add-product page");
-    res.send("<h1>This is the add product page</h1>")
+    // console.log("In add-product page");
+    res.send("<form action=\"/product\" method = \"POST\" ><input type = \"text\" name = \"title\"><button type = \"submit\">Submit</button></form")
     // next(); // allows the request to continue to the next middleware in line 
 });
+
+app.use('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/',(req, res, next)=>{
-    console.log("In '/' middleware");
+    // console.log("In '/' middleware");
     res.send('<h1>Hello from Express!</h1>')
 });
 
