f3adcaff8bf9942c44278985b10d925a98e66d42
SaquibAsghar <saq9516@gmail.com>
2019-10-22 13:46:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express  = require('express');
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("In the middleware");
-    next(); // allows the request to continue to the next middleware in line 
+app.use('/add-product',(req, res, next)=>{
+    console.log("In add-product page");
+    res.send("<h1>This is the add product page</h1>")
+    // next(); // allows the request to continue to the next middleware in line 
 });
-app.use((req, res, next)=>{
-    console.log("In another middleware");
+app.use('/',(req, res, next)=>{
+    console.log("In '/' middleware");
     res.send('<h1>Hello from Express!</h1>')
 });
 
