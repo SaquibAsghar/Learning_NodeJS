4aaaa0ba4bbe02419ea1ef26fee70583d27acd23
SaquibAsghar <saq9516@gmail.com>
2019-10-22 20:20:59 +0530

    Create admin.js
    Always use get/post method instead of use() to get the exact match of the route.

---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const adminRoute = express.Router();
+adminRoute.get('/add-product',(req, res, next)=>{
+    // console.log("In add-product page");
+    res.send("<form action=\"/product\" method = \"POST\" ><input type = \"text\" name = \"title\"><button type = \"submit\">Submit</button></form")
+    // next(); // allows the request to continue to the next middleware in line 
+});
+
+adminRoute.post('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = adminRoute;
