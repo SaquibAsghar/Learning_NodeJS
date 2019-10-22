4ba44cab933310e98408c2bbf382609a7a38e3d8
SaquibAsghar <saq9516@gmail.com>
2019-10-22 20:21:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+// importing 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('/add-product',(req, res, next)=>{
-    // console.log("In add-product page");
-    res.send("<form action=\"/product\" method = \"POST\" ><input type = \"text\" name = \"title\"><button type = \"submit\">Submit</button></form")
-    // next(); // allows the request to continue to the next middleware in line 
-});
-
-app.post('/product',(req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next)=>{
-    // console.log("In '/' middleware");
-    res.send('<h1>Hello from Express!</h1>')
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 
 app.listen(3005);
