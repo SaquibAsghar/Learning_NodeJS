85b9d5e9588d81bd255a38e9e422517a9af8e7a1
SaquibAsghar <saq9516@gmail.com>
2019-10-22 20:56:45 +0530

    Update app.js
    Added the addtional route name '/admin' inside the adminRoute()

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(adminRoute);
+app.use('/admin',adminRoute);
 app.use(shopRoute);
 app.use((req, res, next)=>{
     res.status(404).send("<h1>Page not found!</h1>")
