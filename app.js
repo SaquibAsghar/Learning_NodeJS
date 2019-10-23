6b90ce2aeab15fc8e2f22e9d82611941021f4d23
SaquibAsghar <saq9516@gmail.com>
2019-10-23 12:02:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+const path = require('path');
 const express  = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// importing 
+// importing the routes
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
+const pageNotFoundRoute = require('./routes/pageNotFound(404)');
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true})); // should come at the starting of the app.use()
 
 app.use('/admin',adminRoute);
 app.use(shopRoute);
-app.use((req, res, next)=>{
-    res.status(404).send("<h1>Page not found!</h1>")
-})
+app.use(pageNotFoundRoute);
 
 
 app.listen(3005);
