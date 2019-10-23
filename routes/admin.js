15cdfc63790cb18ba037cabc51d3c82e68cebeb7
SaquibAsghar <saq9516@gmail.com>
2019-10-23 15:38:26 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,17 @@
 const path =require('path')
 const express = require('express');
 
+// importing the path file
+const rootDir = require('../util/path');
+
 const adminRoute = express.Router();
 adminRoute.get('/add-product',(req, res, next)=>{
     // console.log("In add-product page");
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
     // next(); // allows the request to continue to the next middleware in line 
 });
 
-adminRoute.post('/product',(req, res, next)=>{
+adminRoute.post('/add-product',(req, res, next)=>{
     console.log(req.body);
     res.redirect('/'); // this will route to the initial page
 });
