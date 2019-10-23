93d12dbd62dc888d4792cc84a50b95c0ffaa8f0f
SaquibAsghar <saq9516@gmail.com>
2019-10-23 15:38:50 +0530

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 const express = require('express');
+
+// importing the path file
+const rootDir = require('../util/path');
 
 const shopRoute = express.Router();
 
+
 shopRoute.get('/',(req, res, next)=>{
     // console.log("In '/' middleware");
-    res.send('<h1>Hello from Express!</h1>')
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = shopRoute;
